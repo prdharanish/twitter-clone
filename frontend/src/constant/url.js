9ae5cc5a335afa723f2bff0b79e6c5534c71f713
prdharanish <prharanish88@gmail.com>
2025-08-17 19:10:26 +0530

    change the  url

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,2 +1,2 @@
-//export const baseUrl="http://localhost:5000"
-export const baseUrl="https://twitter-clone-5tph.onrender.com/"
+export const baseUrl="http://localhost:3000"
+//export const baseUrl="https://twitter-clone-5tph.onrender.com/"
